a529e53e3a1a32d35cc783271105b1906ce2d112
Davizxh <davihenriqueh15@gmail.com>
2025-02-03 15:00:26 -0300

    Criado o projeto MVC

---
+++ LojaApotheosis/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LojaApotheosis.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
